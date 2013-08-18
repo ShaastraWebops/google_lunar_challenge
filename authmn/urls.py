6787d5e89b24c11e5ba2aa20ea37f85a54c10830
AbdealiJK <abdealikothari@gmail.com>
2013-08-18 19:53:16 +0530

    Admin fixed

--- authmn/urls.py
+++ authmn/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, include, url
 from authmn import views
 
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns('',
 
 	url('^register/$',views.register, name='authmn_register'),
