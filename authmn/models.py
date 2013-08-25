67788fde08321a35971cac1736a011131ac2773b
AbdealiJK <abdealikothari@gmail.com>
2013-08-25 14:29:33 +0530

    Changed to accomodation for girl/boy

--- authmn/models.py
+++ authmn/models.py
@@ -4,11 +4,13 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user         = models.OneToOneField(User)
     team_name    = models.CharField(max_length=20,help_text='please choose a unique team name')
+    
     team_leader  = models.CharField(max_length=20,help_text='(Please enter the name of your Team leader,your team should contain atleast one member and five members at maximum)')
     member_2     = models.CharField(max_length=20,blank=True,help_text='(If you have more than one member in your team,please fill their names)')
     member_3     = models.CharField(max_length=20,blank=True,help_text='(If you have more than one member in your team,please fill their names)')
     member_4     = models.CharField(max_length=20,blank=True,help_text='(If you have more than one member in your team,please fill their names)')
     member_5     = models.CharField(max_length=20,blank=True,help_text='(If you have more than one member in your team,please fill their names)')
+
     team_leader_gender       = models.CharField(max_length = 1, default='M',choices = [('M','Male'),('F','Female')])
     team_leader_age          = models.IntegerField(max_length=3,help_text='(You need to be above 12 to be eligible for participating in shaastra)')
     team_leader_mobilenumber = models.CharField(max_length=12,help_text = '(Please enter your team Mobilenumber)')
@@ -25,6 +27,8 @@ class UserProfile(models.Model):
     team_id = models.CharField(max_length=10)
     centre_for_first_round = models.CharField(max_length=1,default='A',choices = [('A','IITM'),('B','IITD'),('C','VJTI')],help_text='(please enter your firstround allotment preference)',blank=True )
     want_accommodation = models.BooleanField()
+    accomodation_for_boys = models.IntegerField(default=0, help_text='Number of male members in your team')
+    accomodation_for_girls = models.IntegerField(default=0, help_text='Number of female members in your team')
     
     def __unicode__(self):
         return self.user.first_name
